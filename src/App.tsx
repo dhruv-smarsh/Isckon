5dac9673f7e559891b762e04e04fb6ef533baf71
dhruv bhagat <dhruv.bhagat@smarshinfotech.com>
2023-11-02 10:16:25 +0530

    Commit changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Attendance from './Components/Attendance';
-import AppRouting from './AppRouting';
 
 function App() {
   return (
     <>
     <Attendance/>
-    {/* <AppRouting/> */}
     </>
   );
 }
